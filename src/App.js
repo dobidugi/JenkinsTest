f126296f868c3a23e4e9e22aada1cd5b86363f3b
ytlee97 <this@kakao.com>
2023-12-04 16:36:08 +0900

    env file test 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <p>1123</p>
       <header className="App-header">{process.env.REACT_APP_TEST}</header>
     </div>
   );
